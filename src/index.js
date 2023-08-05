13325552bff2c364c796559af3a95b1746474aef
Timofey Kozich <kozich.timofey@gmail.com>
2023-08-05 17:00:42 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const map = new Map(bracketsConfig);
+  let stack = [];
+  for (let i = 0; i < str.length; i++){
+      if(map.get(stack[stack.length - 1]) === str[i]){
+        stack.pop()
+      } else {
+        stack.push(str[i]);
+      }
+  }
+  if(stack.length === 0){
+    return true;
+  } else{
+    return false;
+  }
 }
